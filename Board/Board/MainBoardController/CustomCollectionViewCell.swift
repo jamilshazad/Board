cdba227894d65762eda2c492f334feae8aeddee0
Muhammad Jamil <jamilshazad123@gmail.com>
2017-02-26 11:55:26 +0500

    Added top barMenue and Fixed the hight issue

---
+++ Board/Board/MainBoardController/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  Board
+//
+//  Created by Muhammad Jamil on 25/02/2017.
+//  Copyright © 2017 Shazad. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var menueIcon : UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Board/Board/MainBoardController/KeyboardViewController.swift
+++ Board/Board/MainBoardController/KeyboardViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+private let reuseIdentifier = "CustomMenueCell"
 
 class KeyboardViewController: UIInputViewController {
 
-//    var keyboardView: UIView!
+    @IBOutlet weak var uperMenueCollectionView : UICollectionView!
+    
     @IBOutlet var nextKeyboardButton: UIButton!
+    var heightConstraint: NSLayoutConstraint!
 
     
     override func updateViewConstraints() {
@@ -19,10 +22,30 @@ class KeyboardViewController: UIInputViewController {
         // Add custom view sizing constraints here
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view?.addConstraint(self.heightConstraint)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
          loadInterface()
-        }
+        
+//        let controller = UperMenueCollectionViewController(nibName:"UperMenueCollectionViewController" , bundle:nil) as UperMenueCollectionViewController
+//        addChildViewController(controller)
+//        controller.view.frame = CGRect(x: 50.0, y: 70.0, width: 300.0, height: 40.0)
+//        view.addSubview(controller.view)
+//        controller.didMove(toParentViewController: self)
+        // Register cell classes
+        let nibName = UINib(nibName: "CustomCollectionViewCell", bundle:nil)
+        uperMenueCollectionView!.register(nibName, forCellWithReuseIdentifier: reuseIdentifier)
+       
+        
+        let height : CGFloat = 300
+        
+        self.heightConstraint = NSLayoutConstraint( item:view ?? 200, attribute:.height, relatedBy:.equal, toItem:nil, attribute:.notAnAttribute, multiplier:0.0, constant:height)
+    
+    }
     
     //MARK:loadInterface
     func loadInterface() {
@@ -46,4 +69,31 @@ class KeyboardViewController: UIInputViewController {
     override func textDidChange(_ textInput: UITextInput?) {
      }
 
+}
+
+extension KeyboardViewController : UICollectionViewDelegate,UICollectionViewDataSource{
+    
+     func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 7
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCollectionViewCell
+        cell.menueIcon.image = UIImage(named: "search")
+        // Configure the cell
+        
+        return cell
+    }
+    
+    
+    
+    
 }
